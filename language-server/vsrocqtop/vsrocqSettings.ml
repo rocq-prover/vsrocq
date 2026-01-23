ee61f27f8551b683c6e1d9cab463315b6b470018
Kacper Korban <kacper.f.korban@gmail.com>
2026-01-23 16:55:33 +0100

    Correctly set mcp version

---
+++ language-server/vsrocqtop/vsrocqSettings.ml
@@ -0,0 +1,17 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 VSRocq                                  *)
+(*                                                                        *)
+(*                   Copyright INRIA and contributors                     *)
+(*       (see version control and README file for authors & dates)        *)
+(*                                                                        *)
+(**************************************************************************)
+(*                                                                        *)
+(*   This file is distributed under the terms of the MIT License.         *)
+(*   See LICENSE file.                                                    *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Common settings for VSRocq server components *)
+
+let version = "2.3.4"
