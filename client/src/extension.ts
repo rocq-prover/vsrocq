coq/vscoq

fix: pass a documentUri to the interrupt notification to get state
Without a state we can't kill the current execution

client/src/extension.ts
@@ -208,7 +208,7 @@ export function activate(context: ExtensionContext) {
         registerVscoqTextCommand('addQueryTab', () => searchProvider.addTab());
         registerVscoqTextCommand('collapseAllQueries', () => searchProvider.collapseAll());
         registerVscoqTextCommand('expandAllQueries', () => searchProvider.expandAll());
-        registerVscoqTextCommand('interrupt', () => sendInterrupt(client));
+        registerVscoqTextCommand('interrupt', (editor) => sendInterrupt(editor, client));
         registerVscoqTextCommand('interpretToPoint', (editor) => sendInterpretToPoint(editor, client));
         registerVscoqTextCommand('interpretToEnd', (editor) => sendInterpretToEnd(editor, client));
         registerVscoqTextCommand('stepForward', (editor) => sendStepForward(editor, client));

language-server/vscoqtop/lspManager.ml
@@ -535,7 +535,12 @@ let workspaceDidChangeConfiguration params =
   | Continuous -> run_documents ()
   | Manual -> reset_observe_ids (); ([] : events)
 
-let handle_interrupt () = []
+let handle_interrupt params =
+  let Notification.Client.InterruptParams.{ textDocument } = params in
+  let uri = textDocument.uri in
+  match Hashtbl.find_opt states (DocumentUri.to_path uri) with
+  | None -> log (fun () -> "[interrupt] ignoring event on non existent document"); []
+  | Some { st } -> Dm.DocumentManager.cancel_ongoing_execution st; []
 
 let dispatch_std_request : type a. Jsonrpc.Id.t -> a Lsp.Client_request.t -> (a, error) result * events =
   fun id req ->
@@ -592,7 +597,7 @@ let dispatch_notification =
   | InterpretToEnd params -> log (fun () -> "Received notification: vscoq/interpretToEnd"); coqtopInterpretToEnd params
   | StepBackward params -> log (fun () -> "Received notification: vscoq/stepBackward"); coqtopStepBackward params
   | StepForward params -> log (fun () -> "Received notification: vscoq/stepForward"); coqtopStepForward params
-  | Interrupt -> log (fun () -> "Received notification: vscoq/interrupt"); handle_interrupt ()
+  | Interrupt params -> log (fun () -> "Received notification: vscoq/interrupt"); handle_interrupt params
   | Std notif -> dispatch_std_notification notif
 
 let handle_lsp_event = function

rocq-prover/vsrocq

client/src/extension.ts
@@ -208,7 +208,7 @@ export function activate(context: ExtensionContext) {
         registerVsrocqTextCommand('addQueryTab', () => searchProvider.addTab());
         registerVsrocqTextCommand('collapseAllQueries', () => searchProvider.collapseAll());
         registerVsrocqTextCommand('expandAllQueries', () => searchProvider.expandAll());
-        registerVsrocqTextCommand('interrupt', () => sendInterrupt(client));
+        registerVsrocqTextCommand('interrupt', (editor) => sendInterrupt(editor, client));
         registerVsrocqTextCommand('interpretToPoint', (editor) => sendInterpretToPoint(editor, client));
         registerVsrocqTextCommand('interpretToEnd', (editor) => sendInterpretToEnd(editor, client));
         registerVsrocqTextCommand('stepForward', (editor) => sendStepForward(editor, client));

language-server/vsrocqtop/lspManager.ml
@@ -525,7 +525,12 @@ let workspaceDidChangeConfiguration params =
   | Continuous -> run_documents ()
   | Manual -> reset_observe_ids (); ([] : events)
 
-let handle_interrupt () = []
+let handle_interrupt params =
+  let Notification.Client.InterruptParams.{ textDocument } = params in
+  let uri = textDocument.uri in
+  match Hashtbl.find_opt states (DocumentUri.to_path uri) with
+  | None -> log (fun () -> "[interrupt] ignoring event on non existent document"); []
+  | Some { st } -> Dm.DocumentManager.cancel_ongoing_execution st; []
 
 let dispatch_std_request : type a. Jsonrpc.Id.t -> a Lsp.Client_request.t -> (a, error) result * events =
   fun id req ->
@@ -582,7 +587,7 @@ let dispatch_notification =
   | InterpretToEnd params -> log (fun () -> "Received notification: prover/interpretToEnd"); rocqtopInterpretToEnd params
   | StepBackward params -> log (fun () -> "Received notification: prover/stepBackward"); rocqtopStepBackward params
   | StepForward params -> log (fun () -> "Received notification: prover/stepForward"); rocqtopStepForward params
-  | Interrupt -> log (fun () -> "Received notification: prover/interrupt"); handle_interrupt ()
+  | Interrupt params -> log (fun () -> "Received notification: prover/interrupt"); handle_interrupt params
   | Std notif -> dispatch_std_notification notif
 
 let handle_lsp_event = function