coq/vscoq

feat: add a button and command to send intterupt`

client/src/extension.ts
@@ -40,7 +40,8 @@ import {
     sendInterpretToPoint,
     sendInterpretToEnd,
     sendStepForward,
-    sendStepBackward
+    sendStepBackward,
+    sendInterrupt
 } from './manualChecking';
 import { 
     makeVersionedDocumentId,
@@ -207,6 +208,7 @@ export function activate(context: ExtensionContext) {
         registerVscoqTextCommand('addQueryTab', () => searchProvider.addTab());
         registerVscoqTextCommand('collapseAllQueries', () => searchProvider.collapseAll());
         registerVscoqTextCommand('expandAllQueries', () => searchProvider.expandAll());
+        registerVscoqTextCommand('interrupt', () => sendInterrupt(client));
         registerVscoqTextCommand('interpretToPoint', (editor) => sendInterpretToPoint(editor, client));
         registerVscoqTextCommand('interpretToEnd', (editor) => sendInterpretToEnd(editor, client));
         registerVscoqTextCommand('stepForward', (editor) => sendStepForward(editor, client));

rocq-prover/vsrocq

client/src/extension.ts
@@ -43,7 +43,8 @@ import {
     sendInterpretToPoint,
     sendInterpretToEnd,
     sendStepForward,
-    sendStepBackward
+    sendStepBackward,
+    sendInterrupt
 } from './manualChecking';
 import { DocumentStateViewProvider } from './panels/DocumentStateViewProvider';
 import VsRocqToolchainManager, {ToolchainError, ToolChainErrorCode} from './utilities/toolchain';
@@ -207,6 +208,7 @@ export function activate(context: ExtensionContext) {
         registerVsrocqTextCommand('addQueryTab', () => searchProvider.addTab());
         registerVsrocqTextCommand('collapseAllQueries', () => searchProvider.collapseAll());
         registerVsrocqTextCommand('expandAllQueries', () => searchProvider.expandAll());
+        registerVsrocqTextCommand('interrupt', () => sendInterrupt(client));
         registerVsrocqTextCommand('interpretToPoint', (editor) => sendInterpretToPoint(editor, client));
         registerVsrocqTextCommand('interpretToEnd', (editor) => sendInterpretToEnd(editor, client));
         registerVsrocqTextCommand('stepForward', (editor) => sendStepForward(editor, client));