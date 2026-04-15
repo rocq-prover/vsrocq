575203bbdba1bff1fcd43da5949d0416d3128cfd
shilangyu <xmarcinmarcin@gmail.com>
2026-04-15 23:08:14 +0200

    chore: modernize goal-view-ui

--- client/goal-view-ui/vite.config.ts
+++ client/goal-view-ui/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig( ({mode}) => ({
+// https://vite.dev/config/
+export default defineConfig(({mode}) => ({
   plugins: [react()],
   build: {
     outDir: "build",
     sourcemap: mode === "development" ? "inline" : false,
-    rollupOptions: {
+    rolldownOptions: {
       output: {
         entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
