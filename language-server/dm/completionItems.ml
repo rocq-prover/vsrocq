fc317f47056ef66c2855e84f2eb1dae9e6ce3725
shilangyu <xmarcinmarcin@gmail.com>
2026-05-16 22:45:24 +0200

    Load indices into vsrocq

--- language-server/dm/completionItems.ml
+++ language-server/dm/completionItems.ml
@@ -3,7 +3,9 @@ open Libnames
 open Nametab
 open Printer
 
-type completion_level = 
+open Ppx_yojson_conv_lib.Yojson_conv.Primitives
+
+type completion_level =
   Fully
   | Partially
   | No_completion
@@ -26,7 +28,7 @@ type completion_item = {
   mutable debug_info : string;
 }
 
-let mk_completion_item sigma ref env (c : constr) : completion_item = 
+let mk_completion_item sigma ref env (c : constr) : completion_item =
   {
     ref = ref;
     path = path_of_global ref;
@@ -43,3 +45,68 @@ let pp_completion_item (item : completion_item) : (string * string * string * st
   let path = string_of_path item.path ^ "\n" ^ item.debug_info in
   let typ = Pp.string_of_ppcmds (pr_ltype_env item.env item.sigma item.typ) in
   (Printf.sprintf "%s%s" (symbol_prefix item.completes) name, name, typ, path)
+
+(** Builtin commands, options, and tactics *)
+
+(*
+  Some vernacular is implemented internally in Rocq and not visible as a normal definition
+  that could be added to a completion database through standard scraping. Same goes for
+  Ltac1 tactics.
+
+  To remedy this, we scrape the official documentation of Rocq and create indices of all
+  of the built-in tactics, commands, and options. These indices are then loaded into
+  vsrocqtop to serve these completions to the user.
+*)
+
+(* The grammar of the builtin item as defined in the official grammar *)
+type builtin_syntax =
+  | Literal of {
+      value: string;
+      subscript: string option;
+    }
+  | Reference of {
+      value: string;
+      subscript: string option;
+    }
+  | Alternative of {
+      children: builtin_syntax list;
+    }
+  | Repeat of {
+      min: int;
+      max: int option;
+      separator: string option;
+      children: builtin_syntax list;
+    }
+[@@deriving of_yojson]
+
+type builtin_item = {
+  (* documentation file where this builtin is documented *)
+  documentation_path: string;
+  (* page anchor for this builtin in the documentation *)
+  documentation_anchor: string;
+  (* grammar *)
+  syntax: builtin_syntax list;
+  (* simplified documentation *)
+  documentation: string;
+} [@@deriving of_yojson]
+
+type builtin_index = (string, builtin_item) Hashtbl.t
+
+let load_builtin_index (json_str: string) : builtin_index =
+  let json = Yojson.Safe.from_string json_str in
+  let pairs = Yojson.Safe.Util.to_assoc json in
+  let tbl = Hashtbl.create (List.length pairs) in
+  List.iter (fun (key, json_value) ->
+    Hashtbl.add tbl key (builtin_item_of_yojson json_value)
+  ) pairs;
+  tbl
+
+[%%if rocq = "9.2"]
+let opt_index = load_builtin_index [%blob "indices/9.2/optindex.json"]
+let cmd_index = load_builtin_index [%blob "indices/9.2/cmdindex.json"]
+let tac_index = load_builtin_index [%blob "indices/9.2/tacindex.json"]
+[%%else]
+let opt_index = load_builtin_index "{}"
+let cmd_index = load_builtin_index "{}"
+let tac_index = load_builtin_index "{}"
+[%%endif]
