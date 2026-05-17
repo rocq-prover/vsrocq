7c511bdb39ed2bd6144a07ff7886aaf85e7a168b
shilangyu <xmarcinmarcin@gmail.com>
2026-05-17 12:20:30 +0200

    Map builtins into snippets

--- language-server/dm/completionItems.ml
+++ language-server/dm/completionItems.ml
@@ -79,6 +79,69 @@ type builtin_syntax =
     }
 [@@deriving of_yojson]
 
+(* Pretty print of the builtin syntax item *)
+let rec pp_builtin_syntax (syntax: builtin_syntax) : string =
+  match syntax with
+  | Literal { value; subscript } ->
+      (match subscript with
+      | Some sub -> Printf.sprintf "%s_%s" value sub
+      | None -> value)
+  | Reference { value; subscript } ->
+      (match subscript with
+      | Some sub -> Printf.sprintf "%s_%s" value sub
+      | None -> value)
+  | Alternative { children } ->
+      String.concat "|" (List.map pp_builtin_syntax children)
+  | Repeat { min; max; separator; children } ->
+      let quant = match (min, max) with
+        | (0, Some 1) -> "?"
+        | (0, None) -> "*"
+        | (1, None) -> "+"
+        | (m, Some n) when m = n -> Printf.sprintf "{%d}" m
+        | (m, Some n) -> Printf.sprintf "{%d,%d}" m n
+        | (m, None) -> Printf.sprintf "{%d,}" m
+      in
+      let sep = Option.map (Printf.sprintf "[%s]") separator in
+      let children_str = String.concat " " (List.map pp_builtin_syntax children) in
+      Printf.sprintf "(%s)%s%s" children_str (Option.default "" sep) quant
+
+(* folds over a list by gathering a list while keeping track of an accumulator *)
+let fold_gather f acc items =
+    let (final_acc, mapped) = List.fold_left (fun acc item ->
+      let (next_acc, mapped) = f (fst acc) item in
+      (next_acc, mapped :: snd acc)
+    ) (acc, []) items in
+    (final_acc, List.rev mapped)
+
+(* Snippet syntax following https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#snippet_syntax *)
+let rec builtin_syntax_to_snippet (tabstop: int) (syntax: builtin_syntax) : int * string =
+  match syntax with
+  | Literal { value; subscript } ->
+      (tabstop, match subscript with
+      | Some sub -> Printf.sprintf "%s_%s" value sub
+      | None -> value)
+  | Reference { value; subscript } ->
+      let default = (match subscript with
+      | Some sub -> Printf.sprintf "%s_${%s}" value sub
+      | None -> value) in
+      (tabstop + 1, Printf.sprintf "${%d:%s}" tabstop default)
+  | Alternative { children } ->
+      (* snippet syntax for alternatives is not recursive, so we just show the pretty print of the syntax *)
+      (tabstop+1, Printf.sprintf "${%d|%s|}" tabstop (String.concat "," (List.map pp_builtin_syntax children)))
+  | Repeat { min; max=_; separator; children } ->
+      (* build `min` times carrying on the tabstop *)
+      let (next_tabstop, child_snippets) = fold_gather (fun tabstop _ ->
+        let (next_tabstop, snippets) = fold_gather builtin_syntax_to_snippet tabstop children in
+        (next_tabstop, String.concat " " snippets)
+      ) tabstop (List.init min Fun.id) in
+      let repeat_snippet = String.concat (Option.default "" separator) child_snippets in
+      (next_tabstop, repeat_snippet)
+
+let builtin_syntax_list_to_snippet (syntax_list: builtin_syntax list) : string =
+  let top = snd (fold_gather builtin_syntax_to_snippet 1 syntax_list) in
+  (* we assume all grammars have an implicit "." at the end *)
+  String.concat " " (List.filter (fun s -> s <> "") top) ^ "."
+
 type builtin_item = {
   (* documentation file where this builtin is documented *)
   documentation_path: string;
@@ -90,16 +153,12 @@ type builtin_item = {
   documentation: string;
 } [@@deriving of_yojson]
 
-type builtin_index = (string, builtin_item) Hashtbl.t
+type builtin_index = (string * builtin_item) list
 
 let load_builtin_index (json_str: string) : builtin_index =
   let json = Yojson.Safe.from_string json_str in
   let pairs = Yojson.Safe.Util.to_assoc json in
-  let tbl = Hashtbl.create (List.length pairs) in
-  List.iter (fun (key, json_value) ->
-    Hashtbl.add tbl key (builtin_item_of_yojson json_value)
-  ) pairs;
-  tbl
+  List.map (fun (key, json_value) -> (key, builtin_item_of_yojson json_value)) pairs
 
 [%%if rocq = "9.2"]
 let opt_index = load_builtin_index [%blob "indices/9.2/optindex.json"]
