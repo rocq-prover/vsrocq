7b3a5c114c9463da62b391e41f498d2951b03cf0
shilangyu <xmarcinmarcin@gmail.com>
2026-05-20 22:45:59 +0200

    Remove flushSync for search-ui

--- client/search-ui/src/index.tsx
+++ client/search-ui/src/index.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { flushSync } from "react-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
-// we make sure that the first render is synchronous, allowing us to immediately
-// send events to the webview without worrying about the component not being ready yet.
-flushSync(() => {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-})
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
