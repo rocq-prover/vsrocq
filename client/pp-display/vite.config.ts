71ee715808b84126347f08ad3c8588fde7676520
shilangyu <xmarcinmarcin@gmail.com>
2026-06-18 01:20:39 +0200

    Include css in the pp-display package

--- client/pp-display/vite.config.ts
+++ client/pp-display/vite.config.ts
@@ -2,10 +2,11 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "unplugin-dts/vite";
 import { defineConfig } from "vite";
+import libCss from "vite-plugin-libcss";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), dts({})],
+    plugins: [react(), dts({}), libCss()],
     build: {
         lib: {
             entry: resolve(import.meta.dirname, "src/main.tsx"),
