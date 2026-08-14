397c00b376236ac2dd7bdf9357ad1de5017431f3
Enrico Tassi <Enrico.Tassi@Inria.fr>
2026-08-14 16:58:43 +0200

    fix master

--- flake.nix
+++ flake.nix
@@ -384,23 +384,31 @@
                 ]
                 ++ (with ocamlPackages; [
                   ocaml
-                  yojson_2
+                  yojson
                   findlib
                   ppx_inline_test
                   ppx_assert
                   ppx_sexp_conv
+                  ppx_yojson_conv
                   ppx_deriving
                   ppx_optcomp
                   ppx_import
                   sexplib
-                  (ppx_yojson_conv.override {
-                    ppx_yojson_conv_lib = ppx_yojson_conv_lib.override {
-                      yojson = yojson_2;
-                    };
-                  })
                   lsp
                   sel
-                  memprof-limits
+                  # nixpkgs pins memprof-limits to 0.2.1, which does not build
+                  # against OCaml >= 5 (Gc.Memprof API changes). 0.3.0 does.
+                  (memprof-limits.overrideAttrs (old: {
+                    version = "0.3.0";
+                    src = fetchFromGitLab {
+                      owner = "gadmm";
+                      repo = "memprof-limits";
+                      rev = "v0.3.0";
+                      hash = "sha256-k/uB1jDQtE/PkVPU8zg8cpOmlPttTWVpKerQ0HuWfuI=";
+                    };
+                    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ cppo ];
+                    meta = old.meta // { broken = false; };
+                  }))
                 ]);
               propagatedBuildInputs= (with ocamlPackages;
                 [
