b3524ec221ad5e4e3955d752616e4de31a96bd4e
Enrico Tassi <Enrico.Tassi@Inria.fr>
2026-08-15 12:46:08 +0200

    wait for the tests to finish

--- client/src/test/suite/index.ts
+++ client/src/test/suite/index.ts
@@ -16,15 +16,14 @@ export async function run(): Promise<void> {
     // Add files to the test suite
     files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-    try {
+    return new Promise((resolve, reject) => {
         // Run the mocha test
         mocha.run((failures) => {
             if (failures > 0) {
-                throw new Error(`${failures} tests failed.`);
+                reject(new Error(`${failures} tests failed.`));
+            } else {
+                resolve();
             }
         });
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
+    });
 }
